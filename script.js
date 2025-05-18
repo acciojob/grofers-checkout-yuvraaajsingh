15865066f282197c4ee2f738e425a4ccdbf52592
Yuvraj Singh <84833138+yuvraaajsingh@users.noreply.github.com>
2025-05-18 11:47:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,14 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+   if (document.querySelector(".total-row")) return;
+
+  // Select all elements with class 'price'
+  const priceElements = document.querySelectorAll(".price");
+
+  let total = 0;
+  priceElements.forEach(priceEl => {
+    total += parseFloat(priceEl.textContent);
 };
 
 getSumBtn.addEventListener("click", getSum);
